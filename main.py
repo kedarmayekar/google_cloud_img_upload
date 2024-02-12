21c56bff3b94405917c600cddbf1067d91f1621c
kedarmayekar077 <kedarmayekar077@gmail.com>
2024-02-12 23:41:13 +0530

    Added code for file upload to google cloud

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from google.cloud import storage
 
 def upload_image():
     # Define your credentials (replace with your own)
-    credentials_path = "path/to/your/credentials.json"
+    credentials_path = "credentials/credentials.json"
     client = storage.Client.from_service_account_json(credentials_path)
-
     # Define your bucket name
-    bucket_name = "bucket-name"
+    bucket_name = "gc-photos-storage-dev"
 
     # Define the file path of the image you want to upload
-    file_path = "./images/image.jpg"
+    file_path = "./images/a-beautiful-women-with-green-floral-dress-with-on.jpg"
 
     # Create a blob object
     blob = client.bucket(bucket_name).blob(file_path)
