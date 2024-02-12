498012daf50d15601785e4ddb29769d9b31ada6c
kedarmayekar077 <kedarmayekar077@gmail.com>
2024-02-12 23:45:23 +0530

    Added changes to main.py

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from google.cloud import storage
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+# Define your bucket name
+GOOGLE_CLOUD_BUCKET_NAME = os.environ['GOOGLE_CLOUD_BUCKET_NAME']
 def upload_image():
+    """ Uploads images to google cloud which are stored in images folder in current working directory """
     # Define your credentials (replace with your own)
     credentials_path = "credentials/credentials.json"
     client = storage.Client.from_service_account_json(credentials_path)
-    # Define your bucket name
-    bucket_name = "gc-photos-storage-dev"
 
-    # Define the file path of the image you want to upload
-    file_path = "./images/a-beautiful-women-with-green-floral-dress-with-on.jpg"
+    folder_path = 'images'
+    filenames = os.listdir(folder_path)
 
-    # Create a blob object
-    blob = client.bucket(bucket_name).blob(file_path)
+    for filename in filenames:
+        file_path = f"./{folder_path}/{filename}"
 
-    # Upload the image
-    blob.upload_from_filename(file_path)
+        # Create a blob object
+        blob = client.bucket(GOOGLE_CLOUD_BUCKET_NAME).blob(file_path)
 
-    print(f"Image {file_path} uploaded to {bucket_name}")
+        # Upload the image
+        blob.upload_from_filename(file_path)
+
+        print(f"Image {file_path} uploaded to {GOOGLE_CLOUD_BUCKET_NAME}")
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
